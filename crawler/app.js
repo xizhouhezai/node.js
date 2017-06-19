4e26ae4daadee1959d71b552c658bc03804c1695
yrui <yrui@laixue365.com>
2017-06-19 18:08:08 +0800

    解决跨域请求问题

--- crawler/app.js
+++ crawler/app.js
@@ -3,14 +3,26 @@ var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
 app.get('/', function(req, res){
-    request('http://huaban.com', function (error, response, body) {
+    res.header("Access-Control-Allow-Origin", "*")
+    request('https://muse.huaban.com/', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         $ = cheerio.load(body);//当前的$,它是拿到了整个body的前端选择器
+        var img = $('.home').find('img')
+        var svg = []
+        for(let key in img) {
+            // console.log(img[key].attribs)
+            if (img[key].attribs !== undefined) {
+               if (img[key].attribs.src !== undefined) {
+                   svg.push(img[key].attribs)
+               }
+            }
+        }
+        svg = JSON.stringify(svg)
         res.format({
-            'application/json': function(){
-                res.send({ message: body });
-            },
-        })
+                    'application/json': function(){
+                        res.send({ message: svg });
+                    },
+                })
     //   console.log(response); //我博客的获取用户名
       }else{
          console.log("思密达，没爬取到用户名，再来一次");
