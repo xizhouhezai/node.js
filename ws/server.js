4329951083b5d10ebcadc2936cefb5fc03badf0f
yrui <yrui@laixue365.com>
2017-06-27 18:03:14 +0800

    完成websocket的数据传递

--- ws/server.js
+++ ws/server.js
@@ -1,6 +1,13 @@
 var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({port: 8181})
+var wss = new WebSocketServer({
+    port: 8181,
+    verifyClient: socketVerify
+})
 
+
+function socketVerify (info) {
+    return true
+}
 wss.on('connection', function(ws){
     console.log('client is connected')
     ws.on('message', function (message) {
